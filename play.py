b25711e633747a7a93cf4c90f56aa2483d569169
nlemcke <nathanjlemcke@gmail.com>
2021-09-07 13:51:32 -0700

    defining functions

--- play.py
+++ play.py
@@ -1,3 +1,4 @@
-def wrapper(word, wrap):
-    print(wrap+word+wrap)
-wrapper('name', '*')
+def func(one, three, two=2, four=4):
+    n = str(one) + str(two) + str(three) + str(four)
+    return n
+print(func(1, three=3))
