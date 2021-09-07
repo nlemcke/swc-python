0553a3393844e2dbec85094baa80c13a540193a1
nlemcke <nathanjlemcke@gmail.com>
2021-09-07 15:21:38 -0700

    assertions

--- play.py
+++ play.py
@@ -1,4 +1,5 @@
-def func(one, three, two=2, four=4):
-    n = str(one) + str(two) + str(three) + str(four)
-    return n
-print(func(1, three=3))
+assert range_overlap([ (0.0, 1.0) ]) == (0.0, 1.0)
+assert range_overlap([ (2.0, 3.0), (2.0, 4.0) ]) == (2.0, 3.0)
+assert range_overlap([ (0.0, 1.0), (0.0, 2.0), (-1.0, 1.0) ]) == (0.0, 1.0)
+assert range_overlap([ (0.0, 1.0), (5.0, 6.0) ]) == None
+assert range_overlap([ (0.0, 1.0), (1.0, 2.0) ]) == None
