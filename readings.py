0d2473c0da7b5b89b2d621c1c6fe9cf5c10f1b74
nlemcke <nathanjlemcke@gmail.com>
2021-09-08 12:19:57 -0700

    adding command line flags

--- readings.py
+++ readings.py
@@ -3,10 +3,21 @@ import numpy as np
 
 def main():
     script = sys.argv[0]
-    for filename in sys.argv[1:]:
+    action = sys.argv[1]
+    filenames = sys.argv[2:]
+
+    for filename in filenames:
         data = np.loadtxt(filename, delimiter=',')
-        for row_mean in np.mean(data, axis=1):
-            print(row_mean)
+
+        if action == '--min':
+            values = np.min(data, axis=1)
+        elif action == '--mean':
+            values = np.mean(data, axis=1)
+        elif action == '--max':
+            values = np.max(data, axis=1)
+
+        for val in values:
+            print(val)
 
 if __name__ == '__main__':
     main()
