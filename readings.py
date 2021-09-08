6b22c814ce6aecf7f0df8785eb94e5c64e068ee3
nlemcke <nathanjlemcke@gmail.com>
2021-09-08 12:38:25 -0700

    added assertion

--- readings.py
+++ readings.py
@@ -5,19 +5,23 @@ def main():
     script = sys.argv[0]
     action = sys.argv[1]
     filenames = sys.argv[2:]
-
+    assert action in ['--min', '--mean', '--max'], \
+            'Action is not one of --min, --mean, or --max: ' + action
     for filename in filenames:
-        data = np.loadtxt(filename, delimiter=',')
+        process(filename, action)
 
-        if action == '--min':
-            values = np.min(data, axis=1)
-        elif action == '--mean':
-            values = np.mean(data, axis=1)
-        elif action == '--max':
-            values = np.max(data, axis=1)
+def process(filename, action):
+    data = np.loadtxt(filename, delimiter=',')
 
-        for val in values:
-            print(val)
+    if action == '--min':
+        values = np.min(data, axis=1)
+    elif action == '--mean':
+        values = np.mean(data, axis=1)
+    elif action == '--max':
+        values = np.max(data, axis=1)
+
+    for val in values:
+        print(val)
 
 if __name__ == '__main__':
     main()
